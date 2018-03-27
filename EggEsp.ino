0c67aae1b53363e0b5ba3a67a5f29d8fad094012
Jakob Löw <jakob@m4gnus.de>
2018-03-27 23:20:29 +0200

    implement four instructions for drawing which can be received via tcp over wifi

--- EggEsp.ino
+++ EggEsp.ino
@@ -1,6 +1,36 @@
+#include <ESP8266WiFi.h>
 #include <Servo.h>
 #include "./Stepper.hpp"
 #include "./Draw2D.hpp"
+#include "./config.h"
+
+#ifdef DEBUG
+#define LOG(msg) Serial.print(msg)
+#define LOGLN(msg) Serial.println(msg)
+#else
+#define LOG(msg)
+#define LOGLN(msg)
+#endif
+
+#define OP_MOVE 1
+#define OP_MOVE_REL 2
+#define OP_LINE 3
+#define OP_LINE_REL 4
+
+struct header
+{
+	uint16_t instruction_count;
+	//...
+} __attribute__((packed));
+
+struct instruction
+{
+	uint8_t op;
+	uint8_t spacing1; //reserved
+	int16_t x;
+	int16_t y;
+	uint16_t spacing2; //reserved
+} __attribute__((packed));
 
 void togglePen(bool);
 
@@ -13,6 +43,12 @@ Servo pen;
 Stepper x(D0, D2, D1, D3);
 Stepper y(D5, D7, D6, D8);
 Draw2D draw(x, y, 0, 0, togglePen);
+
+struct instruction *instructions = NULL;
+uint16_t instruction_index;
+uint16_t instruction_count = 0;
+
+WiFiServer server(1337);
 
 void togglePen(bool status)
 {
@@ -47,25 +83,113 @@ void setup()
 {
 	Serial.begin(115200);
 
-	Serial.println("Hello!");
-	delay(5000);
-
-	Serial.println("Initializing servo");
+	LOGLN("Initializing servo");
 	pen.attach(PEN_PIN, 1000, 2000);
 	pen.write(PEN_MOVING);
 	penStatus = false;
 
-	Serial.println("Initializing steppers");
+	LOGLN("Initializing steppers");
 	x.init();
 	x.setSpeed(100);
 	y.init();
 	y.setSpeed(100);
+
+	LOGLN("Initializing WiFi");
+	WiFi.persistent(false);
+	WiFi.mode(WIFI_OFF);
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(WIFI_SSID, WIFI_PSK);
+
+	server.begin();
+
+	togglePen(true);
+	delay(500);
+	togglePen(false);
 }
 
 void loop()
 {
-	draw.lineTo(666, -30);
-	draw.moveTo(-100, 500);
+	for(; instruction_index < instruction_count; instruction_index++)
+	{
+		int16_t x = instructions[instruction_index].x;
+		int16_t y = instructions[instruction_index].y;
 
-	delay(3000);
+		LOG("Instruction: op = ");
+		LOG(instructions[instruction_index].op);
+		LOG(" x = ");
+		LOG(x);
+		LOG(" y = ");
+		LOGLN(y);
+
+		switch(instructions[instruction_index].op)
+		{
+			case OP_MOVE:
+				draw.moveTo(x, y);
+				break;
+
+			case OP_MOVE_REL:
+				draw.move(x, y);
+				break;
+
+			case OP_LINE:
+				draw.lineTo(x, y);
+				break;
+
+			case OP_LINE_REL:
+				draw.line(x, y);
+				break;
+		}
+	}
+
+	if(instructions != NULL)
+	{
+		togglePen(false);
+
+		free(instructions);
+		instructions = NULL;
+	}
+
+	WiFiClient client = server.available();
+	if(client)
+	{
+		LOGLN("Client connected");
+
+		struct header header;
+		if(client.readBytes((char *)&header, sizeof(struct header)) == sizeof(struct header))
+		{
+			LOG("Client sent header with instruction count ");
+			LOGLN(header.instruction_count);
+
+			size_t size = sizeof(struct instruction) * header.instruction_count;
+			instructions = (struct instruction *)malloc(size);
+
+			if(instructions != NULL)
+			{
+				instruction_index = 0;
+				instruction_count = header.instruction_count;
+
+				LOG("Transferring ");
+				LOG(size);
+				LOGLN(" bytes...");
+
+				client.write((uint8_t)0);
+				client.setTimeout(5000);
+
+				if(client.readBytes((char *)instructions, size) != size)
+				{
+					LOGLN("Transmission error!");
+
+					free(instructions);
+					instructions = NULL;
+					instruction_count = 0;
+				}
+				else
+				{
+					LOGLN("Successfully recived instructions");
+				}
+			}
+		}
+
+		client.stop();
+	}
 }
---
+++ config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//#define DEBUG
+
+#define WIFI_SSID "<wifi ssid>"
+#define WIFI_PSK "<wifi password>"
+
+#define DRAW_MIN_X -250
+#define DRAW_MAX_X 250
